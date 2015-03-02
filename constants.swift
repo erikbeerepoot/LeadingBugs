2e35a47731a7921d9a0207032f5398b50eea4128
Erik E. Beerepoot <erikbeerepoot@hush.com>
2015-03-02 09:23:29 -0800

    Created a new constants file

---
+++ constants.swift
@@ -0,0 +1,9 @@
+//
+//  consants.swift
+//  LeadingBugs
+//
+//  Created by Erik E. Beerepoot on 2015-03-02.
+//  Copyright (c) 2015 MediaCore Technologies Inc. All rights reserved.
+//
+
+import Foundation
