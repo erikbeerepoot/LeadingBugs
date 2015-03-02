13c56021d02ae2342c48e148e97a239e34bb9704
Erik E. Beerepoot <erikbeerepoot@hush.com>
2015-03-02 09:10:55 -0800

    Adding actual project files. Last time I just committed the readme. #NeedToReCaffeinate

---
+++ Smiles/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Smiles
+//
+//  Created by Erik E. Beerepoot on 2014-12-18.
+//  Copyright (c) 2014 Dactyl Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
