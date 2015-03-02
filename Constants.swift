dc266d3b46d1998c6fc1346e0d89708838d34f96
Erik E. Beerepoot <erikbeerepoot@hush.com>
2015-03-02 09:09:57 -0800

    Deleted Nexus files from main directory, as they should have been in the library subdirectory

--- Constants.swift
+++ Constants.swift
@@ -1,17 +1,9 @@
 //
-//  Constants.swift
-//  Nexus
+//  constants.swift
+//  LeadingBugs
 //
-//  Created by Erik E. Beerepoot on 2014-12-19.
-//  Copyright (c) 2014 Dactyl Studios. All rights reserved.
+//  Created by Erik E. Beerepoot on 2015-03-02.
+//  Copyright (c) 2015 MediaCore Technologies Inc. All rights reserved.
 //
 
 import Foundation
-
-let kAuthorizationErrorDomain = "authorizationErrorDomain";
-
-let kBotName = "Nexus";
-let kStatusCodeOK = 200;
-
-let kWhiteColor = CGColorCreate(CGColorSpaceCreateDeviceRGB() ,[1.0,1.0,1.0,0.9]);
-let kClearColor = CGColorCreate(CGColorSpaceCreateDeviceRGB() ,[1.0,0.0,0.0,0.0]);
